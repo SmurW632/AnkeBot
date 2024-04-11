93937186a943e2eb2409dfd4c3f91b3fd6913226
Klimater <119484530+klimater@users.noreply.github.com>
2024-04-11 21:28:59 +1000

    Создана папка для клавиатур и поключены кнопки к сообщению

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from aiogram import Bot, Dispatcher
 
 from config import TOKEN
 from app.Handlers import router
+from Forms.VienwForm.Callback import router_callback
+from Forms.VienwForm.ViewForm import router_form
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 async def main():
     dp.include_router(router)
+    dp.include_router(router_callback)
+    dp.include_router(router_form)
     await dp.start_polling(bot)
 
 
