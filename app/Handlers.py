1ade2b133a0c736d9d9eeee80a4df25894a78e76
Fakkabaka <xissme@vk.com>
2024-04-11 23:01:32 +1000

    сделал кнопки и переводчик

--- app/Handlers.py
+++ app/Handlers.py
@@ -1,14 +1,23 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, CallbackQuery, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import Message, CallbackQuery, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
+class StepsForms(StatesGroup):
+    '''
+    Класс в котором лежат указатели состояния
+    '''
+    CHOSENLANGUAGE = State()
+    CHOSENACTIVITY = State()
 
 import app.Keyboard as kb 
-
+import app.TranslateFun.TranslateFun as TF
 router = Router()
+global lang 
 
 @router.message(CommandStart())
-async def cmd_start(message: Message):
-    pass
+async def cmd_start(message: Message, state: FSMContext):
 
     languages = [
         [KeyboardButton(text="English 🇬🇧")],
@@ -20,48 +29,60 @@ async def cmd_start(message: Message):
     languagekb = ReplyKeyboardMarkup(keyboard=languages, resize_keyboard=True,
                                            input_field_placeholder="Choose your language")
     await message.answer("Hello, setup my language!", reply_markup=languagekb)
+    await state.set_state(StepsForms.CHOSENLANGUAGE)
 
-    @router.message()
-    async def choose_language(message: Message):
+@router.message(StepsForms.CHOSENLANGUAGE)
 
-        if message.text in "English":
-            await message.answer("Вы успешно установили язык!")
-            # Добавляем пользователю атрибут английского языка в объект
-        elif message.text in "Russian":
-            await message.answer("Вы успешно установили язык!")
-            # русский
-        elif message.text in "Japanese":
-            await message.answer("Вы успешно установили язык!")
-            # анимешный
-        elif message.text in "Chinese":
-            await message.answer("Вы успешно установили язык!")
-            # китайский
-
-
-@router.message()
-async def choose_Activity(message: Message):
+async def choose_language(message: Message,state: FSMContext):
     
+    if message.text in "English 🇬🇧":
+        lang = "en"
+        await message.answer(await TF.Translation("Вы успешно установили язык!",lang),reply_markup=ReplyKeyboardRemove())
+        
+        # Добавляем пользователю атрибут английского языка в объект
+    elif message.text in "Russian 🇷🇺":
+        lang = 'ru'
+        await message.answer(await TF.Translation("Вы успешно установили язык!",lang),reply_markup=ReplyKeyboardRemove())
+        # русский
+    elif message.text in "Japanese 🇯🇵":
+        lang = 'ja'
+        await message.answer(await TF.Translation("Вы успешно установили язык!",lang),reply_markup=ReplyKeyboardRemove())
+        # анимешный
+    elif message.text in "Chinese 🇨🇳":
+        lang = 'zh-cn'
+        await message.answer(await TF.Translation("Вы успешно установили язык!",lang),reply_markup=ReplyKeyboardRemove())
+        # китайский
+    await state.update_data(lang=lang)
+    await state.set_state(StepsForms.CHOSENACTIVITY)
+
+
+@router.message(StepsForms.CHOSENACTIVITY)
+async def choose_Activity(message: Message,state: FSMContext):
+    data = await state.get_data()
+    lang = await data.get('lang', 'en')
+
     buttons = [
-        [KeyboardButton(text="Начать заполнение")],
-        [KeyboardButton(text="Помочь с заполнением")],
-        [KeyboardButton(text="Помощь")],
-        [KeyboardButton(text="Обратная связь")],
+        [KeyboardButton(text=await TF.Translation("Начать заполнение",lang))],
+        [KeyboardButton(text=await TF.Translation("Помочь с заполнением",lang))],
+        [KeyboardButton(text=await TF.Translation("Помощь",lang))],
+        [KeyboardButton(text=await TF.Translation("Обратная связь",lang))],
     ]
 
     buttonskb = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True,
-                                            input_field_placeholder="What's shall we do?")
-    await message.answer("What's shall we do?", reply_markup=buttonskb)
+                                            input_field_placeholder=await TF.Translation("Что будем делать?",lang))
+    await message.answer(await TF.Translation("Что будем делать?",lang), reply_markup=buttonskb)
 
-    if message.text in "Начать заполнение":
+
+    if message.text in await TF.Translation("Начать заполнение",lang):
         pass
 
-    elif message.text in "Помочь с заполнением":
+    elif message.text in await TF.Translation("Помочь с заполнением",lang):
         pass
 
-    elif message.text in "Помощь":
+    elif message.text in await TF.Translation("Помощь",lang):
         pass
 
-    elif message.text in "Обратная связь":
+    elif message.text in await TF.Translation("Обратная связь",lang):
         pass
 
     
