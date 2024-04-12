f4720714fd136a918b23f3ba0b53178aa5c112b9
Klimater <119484530+klimater@users.noreply.github.com>
2024-04-12 15:11:25 +1000

    Сделал черновое создание формы

--- Forms/ViewForm/ViewFormNoGPT.py
+++ Forms/ViewForm/ViewFormNoGPT.py
@@ -1,11 +1,11 @@
 #Форма заполнения анкеты пользователем без GPT
-from aiogram import Bot, Router
-from aiogram.types import Message
+from aiogram import Bot, Router, F
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
-from Forms.ViewForm.Keyboards.InlineKEyboards import chek_data
+from Forms.ViewForm.Keyboards.InlineKEyboards import *
 
 router_form = Router()
 
@@ -14,11 +14,14 @@ class StepsForms(StatesGroup):
     Класс в котором лежат указатели состояния
     '''
     GET_NAME_FOR_ANKET = State()
+    GET_PHOTO = State()
     GET_DATE = State()
     GET_PLACE = State()
     GET_EPIGRAPH = State()
     GET_BIOGRAPHY = State()
     GET_MEDIA = State()
+    FINISH = State()
+
 
 
 '''
@@ -32,31 +35,38 @@ async def GetName(message: Message, bot: Bot, state: FSMContext):
     await state.set_state(StepsForms.GET_NAME_FOR_ANKET)
 
 @router_form.message(StepsForms.GET_NAME_FOR_ANKET)
-async def SetName(message: Message, state: FSMContext):
-    await state.update_data(FIO = message.text)
-    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_data)
+async def SetName(message: Message):
+    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_name)
+
+@router_form.message(StepsForms.GET_PHOTO)
+async def SetPhoto(message: Message):
+    await message.answer(f"Хотите оставить эту фотографию", reply_markup = chek_photo)
 
 @router_form.message(StepsForms.GET_DATE)
-async def SetName(message: Message, state: FSMContext):
-    await state.update_data(FIO = message.text)
-    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_data)
+async def SetDate(message: Message):
+    await message.answer(f"Провертье правильность заполнения данных:\n {message.text}", reply_markup = chek_date)
 
 @router_form.message(StepsForms.GET_PLACE)
-async def SetName(message: Message, state: FSMContext):
-    await state.update_data(FIO = message.text)
-    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_data)
+async def SetPlace(message: Message, state: FSMContext):
+    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_place)
 
 @router_form.message(StepsForms.GET_EPIGRAPH)
-async def SetName(message: Message, state: FSMContext):
-    await state.update_data(FIO = message.text)
-    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_data)
+async def SetPlace(message: Message, state: FSMContext):
+    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_epigroph)
 
 @router_form.message(StepsForms.GET_BIOGRAPHY)
-async def SetName(message: Message, state: FSMContext):
-    await state.update_data(FIO = message.text)
-    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_data)
+async def SetPlace(message: Message, state: FSMContext):
+    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_biography)
 
-@router_form.message(StepsForms.GET_MEDIA)
-async def SetName(message: Message, state: FSMContext):
-    await state.update_data(FIO = message.text)
-    await message.answer(f"Провертье правильность заполнения данных:\n{message.text}", reply_markup = chek_data)
+@router_form.message(StepsForms.FINISH)
+async def SetPlace(message: Message, state: FSMContext):
+    data_state = await state.get_data()
+    page_memory = f"""{data_state.get("FIO")}
+{data_state.get("PHOTO")}
+дата рождения и смерти: {data_state.get("DATE")}
+место рождение и смерти: {data_state.get("PLACE")}
+{data_state.get("EPIGROPH")}
+{data_state.get("BIOGRAPHY")}
+"""
+    await message.answer(f"Вот ваша страница памяти: {page_memory}\n Хотите заполнить заново", reply_markup = chek_finish)
+    await state.clear()
