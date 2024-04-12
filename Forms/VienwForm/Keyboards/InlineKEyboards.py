75db05552c77b2b4f0f639e8609384177c81ed96
Klimater <119484530+klimater@users.noreply.github.com>
2024-04-12 12:49:18 +1000

    Исправлена ошибка с состояниями и создан макет для работы формы

--- Forms/VienwForm/Keyboards/InlineKEyboards.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import (InlineKeyboardMarkup, InlineKeyboardButton)
-
-chek_data = InlineKeyboardMarkup(inline_keyboard = [
-    [
-        InlineKeyboardButton(text = 'Все правильно', callback_data = 'OK'), 
-        InlineKeyboardButton(text = "Заполнить заново", callback_data = "NOT OK")
-    ]
-])
-
