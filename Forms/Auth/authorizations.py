affd9201259e17a032375365fba27c2b2630bdeb
Mikhail <smurw.developer@gmail.com>
2024-04-13 19:20:39 +1000

    фикс запроса на авторизацию

--- Forms/Auth/authorizations.py
+++ Forms/Auth/authorizations.py
@@ -22,8 +22,6 @@ async def update_memory_page(message: Message):
                 await message.answer("Страница памяти успешно обновлена!")
             else:
                 await message.answer(f"Произошла ошибка при обновлении страницы памяти. Статус: {response.status}")
-<<<<<<< Updated upstream
-=======
                 
 
 async def get_access_token(email, password, device):
@@ -42,6 +40,4 @@ async def get_access_token(email, password, device):
                 return access_token
             else:
                 return None
-
->>>>>>> Stashed changes
-                await message.answer(f"Произошла ошибка при обновлении страницы памяти. Статус: {response.status}")
+            
