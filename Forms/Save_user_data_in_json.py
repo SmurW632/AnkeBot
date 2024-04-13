1c468f7f55fecb995c0bfce24873ae68d77ca2c3
Mikhail <smurw.developer@gmail.com>
2024-04-13 10:55:38 +1000

    удаление файла usurs.json

--- Forms/Save_user_data_in_json.py
+++ Forms/Save_user_data_in_json.py
@@ -1,7 +1,7 @@
 import json
 
 async def save_user_data(user_data):
-    file_name = "users.json"
+    file_name = "JSON/users.json"
 
     with open(file_name, 'w', encoding='utf-8') as file:
         json.dump(user_data, file, ensure_ascii=False, indent=4)
